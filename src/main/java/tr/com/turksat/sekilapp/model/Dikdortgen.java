99f94976bee71272a3b6ec4ad32ca32013f9efad
H-MustafaAy <may84696@gmail.com>
2025-07-22 10:16:32 +0300

    Update Dikdortgen.java

--- src/main/java/tr/com/turksat/sekilapp/model/Dikdortgen.java
+++ src/main/java/tr/com/turksat/sekilapp/model/Dikdortgen.java
@@ -1,19 +1,13 @@
 package tr.com.turksat.sekilapp.model;
-<<<<<<< HEAD
-
 import tr.com.turksat.sekilapp.annotation.TestableClass;
 import tr.com.turksat.sekilapp.annotation.TestableMethod;
 
-=======
->>>>>>> c0746bee659456860b0c0d1e61ceb9ef89fdd8a7
 /**
  * Dikdörtgen sınıfı, Dortgen sınıfından türetilmiş,
  * farklı en ve boya sahip dörtgen şekildir.
  */
-<<<<<<< HEAD
 @TestableClass
-=======
->>>>>>> c0746bee659456860b0c0d1e61ceb9ef89fdd8a7
+
 public class Dikdortgen extends Dortgen {
     /**
      * Dikdörtgen nesnesi oluşturur.
@@ -21,10 +15,9 @@ public class Dikdortgen extends Dortgen {
      * @param en Dikdörtgenin eni
      * @param boy Dikdörtgenin boyu
      */
-<<<<<<< HEAD
+
     
-=======
->>>>>>> c0746bee659456860b0c0d1e61ceb9ef89fdd8a7
+
     public Dikdortgen(int en, int boy) {
         super(en, boy);
     }
@@ -34,10 +27,9 @@ public class Dikdortgen extends Dortgen {
      * @return Dikdörtgenin eni ve boyu ile ilgili açıklayıcı metin
      */
     @Override
-<<<<<<< HEAD
+
     
-=======
->>>>>>> c0746bee659456860b0c0d1e61ceb9ef89fdd8a7
+
     public String toString() {
         return "Dikdörtgen: "+ "en: " +en + " boy: " +boy + "\n";
     }
